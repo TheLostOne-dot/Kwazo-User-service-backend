da57ef3473eb0b7a0de1fce881a9ccd97f54cba6
Stefan Rachev <stef.rachev98@gmail.com>
2022-06-19 04:50:01 +0200

    Messaging

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const db = require("../models/sequelize.index");
 const config = require("../config/auth.config");
+const amqp = require('amqplib/callback_api');
 const User = db.user;
 const Role = db.role;
 const Op = db.Sequelize.Op;
@@ -47,7 +48,7 @@ exports.signin = (req, res) => {
           message: "Invalid Password!",
         });
       }
-      var token = jwt.sign({ id: user.id }, config.secret, {
+      var token = jwt.sign({ username: user.username }, config.secret, {
         expiresIn: 86400, // 24 hours
       });
       var authorities = [];
@@ -60,7 +61,7 @@ exports.signin = (req, res) => {
         return res
           .cookie("access_token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: process.env.NODE_ENV,
           })
           .status(200)
           .send({
@@ -68,8 +69,8 @@ exports.signin = (req, res) => {
             username: user.username,
             email: user.email,
             role: authorities,
-            message: "Logged in successfully"
-          })
+            message: "Logged in successfully",
+          });
       });
     })
     .catch((err) => {
@@ -82,4 +83,62 @@ exports.signout = (req, res) => {
     .clearCookie("access_token")
     .status(200)
     .json({ message: "Successfully logged out!" });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  User.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((user) => {
+      amqp.connect(process.env.AMQP_URL, function (error0, connection) {
+        if (error0) {
+          throw error0;
+        }
+        connection.createChannel(function (error1, channel) {
+          if (error1) {
+            throw error1;
+          }
+          const exchange = "kwazo_exchange";
+          const keyP = "user-deleted.post";
+          const keyC = "user-deleted.comment";
+
+          channel.assertExchange(exchange, "topic", {
+            durable: false,
+          });
+          channel.publish(exchange, keyP, Buffer.from(user.username));
+          console.log(" [x] Sent %s:'%s'", keyP, user.username);
+          channel.publish(exchange, keyC, Buffer.from(user.username));
+          console.log(" [x] Sent %s:'%s'", keyC, user.username);
+        });
+
+        setTimeout(function () {
+          connection.close();
+          // process.exit(0);
+        }, 500);
+      });
+    })
+    .then(() => {
+      User.destroy({
+        where: { id: id },
+      })
+        .then((num) => {
+          if (num == 1) {
+            res.send({
+              message: "User was deleted successfully!",
+            });
+          } else {
+            res.send({
+              message: `Cannot delete User with id=${id}. Maybe Comment was not found!`,
+            });
+          }
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message: "Could not delete User with id=" + id,
+          });
+        });
+    });
 };
