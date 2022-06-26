a1ef7f6fa7eafa7456878a7042a306afb71606d2
Stefan Rachev <stef.rachev98@gmail.com>
2022-06-26 18:28:45 +0200

    Cookie samesite

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -53,7 +53,7 @@ exports.signin = (req, res) => {
           .cookie("access_token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: 'strict'
+            sameSite: 'lax'
           })
           .status(200)
           .send({
