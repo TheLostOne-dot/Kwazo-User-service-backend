396338e96578388d7b7e86f48cb8a81c044b8d36
Stefan Rachev <stef.rachev98@gmail.com>
2022-06-28 16:04:42 +0200

    CPU limit for the demo

--- test.js
+++ test.js
@@ -20,39 +20,45 @@ export default function () {
   const email = "load@mail.com"
 
 
-  const res = http.get("http://20.113.55.136/users/api/test/all");
-  check(res, {
+  const post = http.get("http://20.113.55.136/posts/api/posts");
+  check(post, {
     "status was 200": (r) => r.status === 200,
   });
   sleep(1);
 
-  const user = http.get("http://20.113.55.136/users/api/test/user");
-  check(user, {
-    "status was 403": (r) => r.status === 403,
-  });
-  sleep(1);
+  // const res = http.get("http://20.113.55.136/users/api/test/all");
+  // check(res, {
+  //   "status was 200": (r) => r.status === 200,
+  // });
+  // sleep(1);
 
-  const mod = http.get("http://20.113.55.136/users/api/test/mod");
-  check(mod, {
-    "status was 403": (r) => r.status === 403,
-  });
-  sleep(1);
+  // const user = http.get("http://20.113.55.136/users/api/test/user");
+  // check(user, {
+  //   "status was 403": (r) => r.status === 403,
+  // });
+  // sleep(1);
 
-  const admin = http.get("http://20.113.55.136/users/api/test/admin");
-  check(admin, {
-    "status was 403": (r) => r.status === 403,
-  });
-  sleep(1);
+  // const mod = http.get("http://20.113.55.136/users/api/test/mod");
+  // check(mod, {
+  //   "status was 403": (r) => r.status === 403,
+  // });
+  // sleep(1);
+
+  // const admin = http.get("http://20.113.55.136/users/api/test/admin");
+  // check(admin, {
+  //   "status was 403": (r) => r.status === 403,
+  // });
+  // sleep(1);
 
 
-  const register = http.post("http://20.113.55.136/users/api/auth/signup", { username: username, password: password, email: email})
-  check(register, {
-    "status was 400": (r) => r.status === 400,
-  })
+  // const register = http.post("http://20.113.55.136/users/api/auth/signup", { username: username, password: password, email: email})
+  // check(register, {
+  //   "status was 400": (r) => r.status === 400,
+  // })
 
 
-  const login = http.post("http://20.113.55.136/users/api/auth/signin", { username: username, password: password})
-  check(login, {
-    "status was 200": (r) => r.status === 200,
-  })
+  // const login = http.post("http://20.113.55.136/users/api/auth/signin", { username: username, password: password})
+  // check(login, {
+  //   "status was 200": (r) => r.status === 200,
+  // })
 }
